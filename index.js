03c5c6817b2b6d800bb80e43ee58ed29bcb54df1
Varshita Mishra <varshitam06@gmail.com>
2025-11-13 19:29:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+document.querySelectorAll("button");
+var i;
+var n = document.querySelectorAll(".drum").length;
+
+function playAudio(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    case "l":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    default:
+      console.log(buttonInnerHtml);
+  }
+}
+
+function addAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
+
+for (i = 0; i < n; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    var buttonInnerHtml = this.innerHTML;
+    playAudio(buttonInnerHtml);
+    addAnimation(buttonInnerHtml);
+  });
+}
+
+document.addEventListener("keydown", function (event) {
+  playAudio(event.key);
+  addAnimation(event.key);
+});
